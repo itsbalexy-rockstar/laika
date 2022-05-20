3afc97e1b55651c0d44d07460f67f8f5f7e2edd6
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-05-19 20:03:12 -0500

    data colums added each table

--- database/migrations/2022_05_20_003443_create_farmapets_table.php
+++ database/migrations/2022_05_20_003443_create_farmapets_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('farmapets', function (Blueprint $table) {
             $table->id();
+            $table->string('imagen');
+            $table->string('nombre');
+            $table->integer('puntaje');
+            $table->integer('precio');
             $table->timestamps();
         });
     }
