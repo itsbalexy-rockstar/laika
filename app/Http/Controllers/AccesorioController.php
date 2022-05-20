a00ee25df241baa85dd4c064d6a680fdb522d46f
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-05-19 19:37:42 -0500

    mfr migrations each table

---
+++ app/Http/Controllers/AccesorioController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Accesorio;
+use Illuminate\Http\Request;
+
+class AccesorioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Accesorio  $accesorio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Accesorio $accesorio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Accesorio  $accesorio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Accesorio $accesorio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Accesorio  $accesorio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Accesorio $accesorio)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/AlimentoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alimento;
+use Illuminate\Http\Request;
+
+class AlimentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Alimento  $alimento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Alimento $alimento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Alimento  $alimento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Alimento $alimento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Alimento  $alimento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Alimento $alimento)
+    {
+        //
+    }
+}
