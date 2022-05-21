ca4357369f59058ef4fb18fbb584db33b1773c69
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-05-21 10:40:13 -0500

    layout basico

---
+++ resources/js/components/Navbar/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container-fluid bg fixed">
+        <!-- primer navbar -->
+            <NavbarInfo />
+        <!-- segundo navbar -->
+            <NavbarSearch />
+        <!-- tercer navbar -->
+            <NavbarOptions />
+    </div>
+</template>
+<script>
+import NavbarInfo from './NavbarInfo.vue'
+import NavbarSearch from './NavbarSearch.vue'
+import NavbarOptions from './NavbarOptions.vue'
+
+export default {
+    components: {
+        NavbarInfo,
+        NavbarSearch,
+        NavbarOptions
+    },
+}
+</script>
+
+<style>
+.bg {
+    background-color: #5f3e85;
+}
+.bg.darken-4 {
+    filter: brightness(0.6);
+}
+.bg-btn-vet {
+    background-color: #496aff;
+}
+.text-color {
+    color: #5f3e85;
+}
+.text-color:hover{
+    color: #5f3e85;
+}
+</style>
